b3ac07e639d4c87b70cdf3a87fa655716714944e
apahomovs <alexanderpahomovs@gmail.com>
2024-11-08 23:15:37 -0600

    update

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -3,5 +3,6 @@ output "id" {
 }
 
 output "igw_id" {
-  value = var.create_attach_igw ? aws_internet_gateway.igw[0].id : null
+  value       = length(aws_internet_gateway.igw) > 0 ? aws_internet_gateway.igw[0].id : null
+  description = "ID of the Internet Gateway attached to the VPC, or null if not created"
 }
