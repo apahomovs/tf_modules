1c117355e8a9ab8af3e19ca50867efca432a2408
apahomovs <alexanderpahomovs@gmail.com>
2024-11-12 11:14:23 -0600

    update

--- rt_table/main.tf
+++ rt_table/main.tf
@@ -1,9 +1,13 @@
 resource "aws_route_table" "rtb" {
-    vpc_id = var.vpc_id
+  vpc_id = var.vpc_id
+
+  # Define the route with conditional assignment
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = var.gateway_id == null ? null : var.nat_gateway_id 
-    nat_gateway_id = var.nat_gateway_id == null ? null : var.nat_gateway_id
+
+    # Use gateway_id if var.gateway_id is set, otherwise use nat_gateway_id
+    gateway_id     = var.gateway_id != null ? var.gateway_id : null
+    nat_gateway_id = var.gateway_id == null && var.nat_gateway_id != null ? var.nat_gateway_id : null
   }
 
   tags = {
@@ -11,14 +15,15 @@ resource "aws_route_table" "rtb" {
   }
 }
 
+resource "aws_route_table_association" "assoc" {
+  count          = length(var.subnets)
+  subnet_id      = var.subnets[count.index]
+  route_table_id = aws_route_table.rtb.id
+}
+
+
 # ? is a ternary operator (elvis operator)
 # expression ? if_true : if_false
 # 100 > 99 ? apple : banana
 # variable Name == Steve
 # name != David ? 100 : 200
-
-resource "aws_route_table_association" "assoc" {
-  count = length(var.subnets)
-  subnet_id = var.subnets[count.index]
-  route_table_id = aws_route_table.rtb.id
-}
