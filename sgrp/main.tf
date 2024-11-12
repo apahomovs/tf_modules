2a4fdce98a3d382fc1ec05d16a25163492399fa9
apahomovs <alexanderpahomovs@gmail.com>
2024-11-12 16:33:03 -0600

    update

--- sgrp/main.tf
+++ sgrp/main.tf
@@ -3,10 +3,16 @@ name = var.name
 description = var.desciption
 vpc_id = var.vpc_id
 
+egress {
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
 tags = {
   Name = var.sg_tag
 }
-
 }
 
 resource "aws_security_group_rule" "my_rule" {
